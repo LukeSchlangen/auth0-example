73c049b46dcff495a2e04756423a2e7931237c7e
Luke Schlangen <lukeschlangen@gmail.com>
2016-09-27 14:19:41 -0500

    Adding Auth0

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,22 @@
 console.log('sourcing client.js is good!')
 
 var myApp = angular.module('myApp', []);
+var lock = new Auth0Lock( '10mqdLAK2yESHwgKh4y72DAXEv3TeV1M', 'lukeschlangen.auth0.com');
+// log out url, from Auth0
+var logOutUrl = 'https://lukeschlangen.auth0.com/v2/logout';
 
 myApp.controller('authController', ['$scope', '$http', function($scope, $http){
   console.log('authController at the ready!');
   $scope.content = "Here is some content from our controller!";
   $scope.showThings = true;
+  $scope.logIn = function(){
+    console.log('in logIn function');
+    lock.show(function(err, profile, token){
+      if(err){
+        console.log('error:', err);
+      }else{
+        console.log('Auth0 success, Profile: ', profile);
+      }
+    });
+  };
 }]);
